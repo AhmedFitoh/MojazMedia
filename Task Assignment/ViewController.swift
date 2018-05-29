928cead25cff0d9415c1ebfd22ffede43cb33d0f
AhmedFitoh <ahmed.fitoh.zakrya@protonmail.com>
2018-05-29 17:02:23 +0200

    Initial Commit

---
+++ Task Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task Assignment
+//
+//  Created by Ahmed Fitoh on 5/29/18.
+//  Copyright © 2018 mojazapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
