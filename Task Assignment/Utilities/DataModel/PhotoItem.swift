489116b2dfc14c165cde6c10de10202f2a500be6
AhmedFitoh <ahmed.fitoh.zakrya@protonmail.com>
2018-05-29 17:52:22 +0200

    Data model added

---
+++ Task Assignment/Utilities/DataModel/PhotoItem.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoItem.swift
+//  Task Assignment
+//
+//  Created by Ahmed Fitoh on 5/29/18.
+//  Copyright © 2018 mojazapp. All rights reserved.
+//
+
+import Foundation
+
+
+struct PhotoItem {
+    var albumId      : Int
+    var id           : Int
+    var title        : String
+    var photoUrl     : String
+    var thumbnailUrl : String
+}
---
+++ Task Assignment/ViewControllers/HomeScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Task Assignment
+//
+//  Created by Ahmed Fitoh on 5/29/18.
+//  Copyright © 2018 mojazapp. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+
+}
+
